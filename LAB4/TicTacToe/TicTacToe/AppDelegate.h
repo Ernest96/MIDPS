353f7ab1ec508ddcc5b09c370254621360e51e19
Ernest96 <bitca@inbox.ru>
2017-04-14 20:07:09 +0300

    LAB4 added

---
+++ LAB4/TicTacToe/TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Daniel Prvornii on 10.04.17.
+//  Copyright © 2017 Bitca Ernest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
